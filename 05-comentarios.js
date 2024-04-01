d0c49af146e4a9b2071784f8a52317e803f7d388
ManuelManodes <63275720+ManuelManodes@users.noreply.github.com>
2024-04-01 18:01:10 -0300

    Carga manual avance

---
+++ 05-comentarios.js
@@ -0,0 +1,9 @@
+// Esto es un comentario
+/*
+Todo lo que escribamos dentro del slash y los asteriscos
+será ignorado.
+*/ 
+
+let numero = 42; // Este es otro comentario
+
+console.log(numero);
---
+++ 07-arrays.js
@@ -0,0 +1,9 @@
+let animales = ['lunita', 'tuti'];
+
+console.log(animales);
+console.log(animales[0]);
+animales[2] = 'almibita';
+
+console.log(animales[7]);
+console.log(typeof animales);
+console.log(animales.length);
